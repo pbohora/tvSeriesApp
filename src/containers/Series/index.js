9101da7a107150aa1ecb2e2a30dccdbc82c5e8c5
pbohora <pradeepbohora26@gmail.com>
2018-09-02 16:54:06 +0300

    Refactor list component

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import SeriesList from '../../components/SeriesList';
 
 class Series extends Component {
     state = {
         anime: []
     }
 
-    componentDidMount (){
-        fetch('http://api.tvmaze.com/search/shows?q=girls')
+      onSeriesInputChange = e =>{
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
         .then(response => response.json())
         .then(json => this.setState({anime:json}));
       }
     render(){
         return(
-            <div> The length of the anime array- {this.state.anime.length}</div> 
+            <div> 
+                The length of the anime array- {this.state.anime.length}
+                <div>
+                    <input type="text" onChange={this.onSeriesInputChange} />
+                </div>
+                <SeriesList list= {this.state.anime} />
+            
+            </div> 
+            
         );
     }
 }
